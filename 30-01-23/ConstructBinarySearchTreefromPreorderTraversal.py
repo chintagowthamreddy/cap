b2d286183b662d01cc0738ac36f091450c8d64b0
chintagowthamreddy <84315634+chintagowthamreddy@users.noreply.github.com>
2023-01-30 21:22:49 +0530

    Create ConstructBinarySearchTreefromPreorderTraversal.py

---
+++ 30-01-23/ConstructBinarySearchTreefromPreorderTraversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def bstFromPreorder(self, A: List[int]) -> Optional[TreeNode]:
+        if not A:
+            return None
+        root = TreeNode(A[0])
+        i = bisect.bisect(A, A[0])
+        root.left = self.bstFromPreorder(A[1:i])
+        root.right = self.bstFromPreorder(A[i:])
+        return root
