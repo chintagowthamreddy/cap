27492297f8f19aad728f0e3012cfcf51df146b51
chintagowthamreddy <84315634+chintagowthamreddy@users.noreply.github.com>
2023-02-03 10:12:32 +0530

    Create topview.py

---
+++ 02-02-23/topview.py
@@ -0,0 +1,15 @@
+def topView(root):
+    #Write your code here
+    d={}
+    q=[]
+    q.append((root,0))
+    while(q):
+        i=q.pop(0)
+        if i[1] not in d:
+            d[i[1]]=i[0].info
+        if i[0].left:
+            q.append((i[0].left,i[1]-1))
+        if i[0].right:
+            q.append((i[0].right,i[1]+1))  
+    for i,j in sorted(d.items()):
+        print(str(j),end=" ")
